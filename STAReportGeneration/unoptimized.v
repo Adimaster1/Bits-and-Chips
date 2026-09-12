60b5ab1f3a1ab28796b148b7753bf011fe8fa38d
Arnav Desai <digitalmind63@yahoo.com>
2026-09-12 21:36:13 +0530

    Added latest version
    Latest version of project on my end is added in nebula_rtl_project folder including env file.

--- STAReportGeneration/unoptimized.v
+++
@@ -1,9 +0,0 @@
-module heavy_calculator (
-    input wire clk,
-    input wire [31:0] a, b, c, d, e, f, g, h,
-    output reg [31:0] sum_out
-);
-    always @(posedge clk) begin
-        sum_out <= a + b + c + d + e + f + g + h; 
-    end
-endmodule
